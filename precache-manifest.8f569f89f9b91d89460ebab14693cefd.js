5085719e4199af6aa61e0f613415bd878a54e235
KareemHisham <kareem.hisham.ahmed@gmail.com>
2019-10-09 13:30:31 +0200

    Updates

---
+++ precache-manifest.8f569f89f9b91d89460ebab14693cefd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a834903e3cf5d1c7e72a149bd49a3758",
+    "url": "/Reminder-App/index.html"
+  },
+  {
+    "revision": "50068bc84cf29b3fd63e",
+    "url": "/Reminder-App/static/css/2.5f589258.chunk.css"
+  },
+  {
+    "revision": "741bf92557a526624228",
+    "url": "/Reminder-App/static/css/main.cfaee5ba.chunk.css"
+  },
+  {
+    "revision": "50068bc84cf29b3fd63e",
+    "url": "/Reminder-App/static/js/2.c071774a.chunk.js"
+  },
+  {
+    "revision": "741bf92557a526624228",
+    "url": "/Reminder-App/static/js/main.6c9061e1.chunk.js"
+  },
+  {
+    "revision": "4094d1e54d9ec0360da3",
+    "url": "/Reminder-App/static/js/runtime-main.ab05a2aa.js"
+  },
+  {
+    "revision": "2c51f81ea0f98a3238ca69bda8990a5f",
+    "url": "/Reminder-App/static/media/Reminders.2c51f81e.png"
+  }
+]);
